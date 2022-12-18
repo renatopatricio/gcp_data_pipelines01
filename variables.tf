ca96868c70dbc4863c6eab0cdefb90e9ccd0ed15
renatopatricio <renatopatricio@hotmail.com>
2022-12-18 11:37:10 -0300

    Criando um bucket simples

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "stack-storage-terraform-pipeline01" {
+  type        = string
+  description = "GCP Terraform lab Stack 01"
+}
+
+variable "us-central1" {
+  type    = string
+  default = "Google Cloud location of resources"
+}
