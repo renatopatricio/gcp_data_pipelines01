3554158f33358d075af60b8aefac4ea2689a9450
renatopatricio <renatopatricio@hotmail.com>
2022-12-21 23:19:10 -0300

    Criando dataset, table e buckets3

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ module "bigquery-dataset-gasolina" {
         },
         range_partitioning = null,
         expiration_time = null,
-        clustering      = ["produto","regiao_sigla", "estado_sigla"],
+        clustering      = ["produto","regiao_sigla", "estado_sigla"],  
         labels          = {
           name    = "stack_data_pipeline"
           project  = "gasolina"
